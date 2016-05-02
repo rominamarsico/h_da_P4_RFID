cd9ff73d322288975169c9f050af42ebbc1c87a6
Romina Marsico <rominamarsico@users.noreply.github.com>
2016-05-02 15:20:26 +0200

    Added files via upload
    This sketch reads the tag number and prints it to the serial monitor.

---
+++ Ausgabe_Kartennummer/Ausgabe_Kartennummer.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop()
+{
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  long code=0;
+
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    code=((code+mfrc522.uid.uidByte[i])*10);
+  }
+
+  Serial.print("Die Kartennummer lautet:");
+  Serial.println(code);
+}
